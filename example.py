e1ec34e582e7d274dc7e6b677448c6b1eefe3612
JP-Vela <jangofett122003@gmail.com>
2023-06-05 00:55:31 -0700

    Added example image

--- example.py
+++ example.py
@@ -4,10 +4,14 @@ carter_search = CarterSearch()
 
 # Get a raw output from DuckDuckGo
 print("Raw Data")
-output = carter_search.ask_raw("How much does the iphone 14 cost?")
+output = carter_search.ask_raw("How fast is the apple m2 chip?")
 print(output['output'])
 
 # Get a summarized answer (using carter and prompting)
 print("\nClean Answer")
-output = carter_search.ask_answer("How much does the iphone 14 cost?")
+output = carter_search.ask_answer("How fast is the apple m2 chip?")
 print(output['output'])
+
+
+# Using your own Carter agent
+custom_search = CarterSearch("API KEY", "PLAYER ID", "POST PROMPT")
